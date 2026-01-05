43c8482e2494640d1bbbdd28057908a1c731de3f
manhar98 <manharzapdiya9898@gmail.com>
2026-01-05 21:41:45 +0530

    Add files via upload

--- project6.c
+++ project6.c
@@ -1,55 +1,56 @@
 #include <stdio.h>
-int main(){
-   char str[100], rev[100];
-    int j = 0, len = 0, flag = 1;
 
-    printf("Enter any string: ");
-    scanf("%s", str);
-
-    while (str[len] != '\0') {
-        len++;
+int main() {
+    
+    char a[20];
+    int flag = 1;
+    int len = 0;
+    
+    printf("Enter the word :- ");
+    scanf("%s",&a);
+    
+    for(int i = 0 ; a[i] != '\0' ; i++){
+        len = i;
     }
-
-    for (int i = len - 1; i >= 0; i--) {
-        rev[j] = str[i];
-        j++;
-    }
-    rev[j] = '\0'; 
-
-    for (int i = 0; i < len; i++) {
-        if (str[i] != rev[i]) {
+    
+    for(int i = 0 ; a[i] != '\0' ; i++){
+        if(a[i] != a[len]){
             flag = 0;
             break;
         }
+        len--;
+    }
+    
+    if(flag){
+        printf("String is palindrome.\n");
     }
 
-    if (flag == 1)
-        printf("%s = True (Palindrome)\n", str);
-    else
-        printf("%s = False (Not Palindrome)\n", str);
+    else{
+        printf("String is not palindrome.\n");
+    }
+    
+    printf("Frequency of each letter :- \n");
+    
+    for(int i = 0 ; a[i] != '\0' ; i++){
+        int count = 0;
+        int ac = 0;
 
-
-
-
-
-    // printf("Q-2 Frequency Counter.");
-    // char str[100];
-    // int freq[26] = {0};
-    // int i;
-
-    // printf("Enter any string: ");
-    // scanf("%s", str);
-
-    // for (i = 0; str[i] != '\0'; i++){
-    //     if (str[i] >= 'a' && str[i] <= 'z'){
-    //         freq[str[i] - 'a']++;
-    //     }
-    // }
-
-    // printf("\nFrequency of each letter:\n");
-    // for (i = 0; i < 26; i++){
-    //     if (freq[i] != 0){
-    //         printf("%c => %d\n", i + 'a', freq[i]);
-    //     }
-    // }
+        for (int k = 0; k < i; k++) {
+            if (a[k] == a[i]) {
+                ac = 1;
+                break;
+            }
+        }
+    
+        
+        if (ac)
+            continue;
+            
+        for(int j = 0 ; a[j] != '\0' ; j++){
+            if(a[j] == a[i]){
+                count++;
+            }
+        }
+        printf("%c :- %d\n",a[i],count);
+    }
 }
